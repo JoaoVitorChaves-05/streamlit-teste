8b72fe1ffeb9133fd5aebaf60da1564e27e359e2
João Vitor Chaves <joaovmanciochaves@gmail.com>
2025-03-21 10:21:58 -0300

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Streamlit, withStreamlitConnection } from "streamlit-component-lib";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import viteLogo from "./vite.svg";
 import "./App.css";
 
 function App({ args }) {
